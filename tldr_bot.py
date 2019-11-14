3bd6314c5c12a4654b0c0deee06aeac93ed81b0e
Marco Bonelli <marco@mebeim.net>
2019-11-14 22:56:34 +0100

    Switch to Python 3, add new endpoint, refactor code
    Changes in detail:
    
     1. Switch to Python 3.
     2. Rename `post_comment.py` to `tldr_bot.py`.
     3. Change main endpoint from `/` to `/comment` (old one is still
        kept for compatibility, will be removed once we update the
        travis scripts on the pages repo).
     4. Add new endpoint `/comment/once` which does the same as
        `/comment`, but first checks if `tldr-bot` hasn't already
        commented on the PR.
     5. Rename the `/test` endpoint to `/status`.
     6. Change HTTP response code for malformed request from 500 (server
        error) to 400 (client error).
     7. Remove the unneeded `GITHUB_URL` environment variable.
     8. Refactor code (reorder imports, indentation, spacing, etc.)

---
+++ tldr_bot.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import requests
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+def post_comment(issue_id, body):
+    url     = '{}/repos/{}/issues/{}/comments'.format(GITHUB_API_URL, REPO_SLUG, issue_id)
+    headers = {'Authorization': 'token ' + BOT_TOKEN}
+    data    = {'body': body}
+
+    return requests.post(url, json=data, headers=headers)
+
+
+def check_already_commented(issue_id):
+    url  = '{}/repos/{}/issues/{}/comments'.format(GITHUB_API_URL, REPO_SLUG, issue_id)
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        # GitHub API error, let's say True in this case to avoid problems.
+        return True
+
+    comments = resp.json()
+    print(comments)
+    return any(c['user']['login'] == 'tldr-bot' for c in comments)
+
+
+@app.route('/', methods=['POST']) # old route, to be removed
+@app.route('/comment', methods=['POST'])
+@app.route('/comment/once', methods=['POST'])
+def comment():
+    data = request.get_json()
+
+    if data is None:
+        return make_response('Invalid JSON or inappropriate Content-Type.', 400)
+
+    pr_id        = data.get('pr_id')
+    comment_body = data.get('body')
+
+    # Check if request is valid.
+    if pr_id is None or comment_body is None:
+        return make_response('Missing required JSON fields', 400)
+
+    # If needed, check if already commented.
+    if request.path == '/comment/once' and check_already_commented(pr_id):
+        return make_response('already commented', 200)
+
+    # Post comment.
+    resp = post_comment(pr_id, comment_body)
+
+    if resp.status_code != 201:
+        return make_response(resp.text, 500)
+
+    return make_response('success', 200)
+
+
+@app.route('/status', methods=['GET'])
+def status():
+    # Health check entpoint for monitoring purposes.
+    return make_response('ok', 200)
+
+
+################################################################################
+
+GITHUB_API_URL = 'https://api.github.com'
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+REPO_SLUG = os.getenv('REPO_SLUG')
+
+if BOT_TOKEN is None or REPO_SLUG is None:
+  print('Needed environment variables are not set.')
+  sys.exit(1)
