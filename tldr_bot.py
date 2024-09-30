c95164fff0943a3091a10ba569397130f44116f5
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-09-30 19:43:32 +0200

    tldr-bot: use BOT_USERNAME everywhere

--- tldr_bot.py
+++ tldr_bot.py
@@ -33,7 +33,7 @@ def previous_comment(issue_id, identifier):
 
     comments = resp.json()
     for comment in comments:
-        if comment['user']['login'] == 'tldr-bot' and identifier in comment['body']:
+        if comment['user']['login'] == BOT_USERNAME and identifier in comment['body']:
             return comment['id']
     return None
 
